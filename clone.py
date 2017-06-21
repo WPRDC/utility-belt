1283db4f49061f8bfadc6f3ca058456d0094e98b
drw <drw@users.noreply.github.com>
2017-06-21 11:56:51 -0400

    Determine and print names of resources and packages

--- clone.py
+++ clone.py
@@ -47,18 +47,21 @@ def investigate_id(site,source_id,API_key):
     try: # if its_a_resource(source_id):
         metadata = ckan.action.resource_show(id=source_id)
         source_type = 'resource'
+        source_name = metadata['name']
     except ckanapi.errors.NotFound:
         resource_alias = dealias(site,source_id,API_key)
         if resoure_alias is not None:
             source_type = 'resource alias'
             source_id = resource_alias
+            source_name = ckan.action.resource_show(id=resource_alias)['name']
         else:
             try:
                 metadata = ckan.action.package_show(id=source_id)
                 source_type = 'package'
+                source_name = metadata['title']
             except:
-                return None, None
-    return source_type, source_id 
+                return None, None, None
+    return source_type, source_id, source_name
 
 def main():
     path = os.path.dirname(os.path.realpath(__file__))
@@ -67,20 +70,25 @@ def main():
     site, API_key, settings = fire_grappling_hook(filepath='ckan_settings.json',server='Live'):
 
     # Is it a resource or a package or a resource alias?
-    source_type, source_id = investigate_id(site,source_id,API_key)
+    source_type, source_id, source_name = investigate_id(site,source_id,API_key)
 
     destination_id = None
     if len(sys.argv) > 2:
         destination_id = sys.argv[2]
 
-    question = "Are you sure you want to clone {} {}".format(source_type, source_id)
+    question = "Are you sure you want to clone {} {} ({})".format(source_type, source_id, source_name)
     if source_type != 'package':
         if destination_id is None:
             question += " to a completely new package?"
         else:
-            question += " to destination ID {}".format(destination_id)
+            question += " to destination ID {} ({})?".format(destination_id, desintation_name)
     response = query_yes_no(question, default="yes")
     if response:
+        if destination_id is None:
+            # Create a new package 
+
+            destination_id = ckan.action.package_create
+            destination_type = 'package'
         # Do the cloning
 
     server = "Live"
